327a5e5b277e266ab3268dbad5c907ea9779a90a
FXY21638 <2163833350@qq.com>
2026-03-20 10:52:00 +0800

    增加了sinx函数的打印

--- Python_Study_Local/helloworld.py
+++ Python_Study_Local/helloworld.py
@@ -1,2 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("Hello, World!")
 print("信号与系统学习")
+
+# 生成 x 数据
+x = np.linspace(0, 2 * np.pi, 100)  # 100 个点
+
+# 计算 y = sin(x)
+y = np.sin(x)
+
+# 绘制 sin(x) 图像
+plt.figure(figsize=(8, 4))
+plt.plot(x, y, label='sin(x)')
+plt.title('y = sin(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.grid(True)
+plt.show()
